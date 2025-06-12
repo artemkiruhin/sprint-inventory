a4054b3cccd216caf71e517c73fc098d93eca226
artemkiruhin <temkaunik@gmail.com>
2025-06-12 19:49:42 +0300

    upd: status log added itemId

--- backend/SprintInventory/SprintInventory.Core/Models/Entities/InventoryItemStatusLogEntity.cs
+++ backend/SprintInventory/SprintInventory.Core/Models/Entities/InventoryItemStatusLogEntity.cs
@@ -3,17 +3,20 @@
 public class InventoryItemStatusLogEntity
 {
     public Guid Id { get; set; }
+    public Guid ItemId { get; set; }
     public ItemStatus StatusFrom { get; set; }
     public ItemStatus StatusTo { get; set; }
     public Guid? UserId { get; set; }
     public DateTime Timestamp { get; set; }
     
     public virtual UserEntity User { get; set; } = null!;
+    public virtual InventoryItemEntity Item { get; set; } = null!;
 
-    public static InventoryItemStatusLogEntity Create(ItemStatus statusFrom, ItemStatus statusTo, Guid? userId)
+    public static InventoryItemStatusLogEntity Create(Guid itemId, ItemStatus statusFrom, ItemStatus statusTo, Guid? userId)
         => new()
         {
             Id = Guid.NewGuid(),
+            ItemId = itemId,
             StatusFrom = statusFrom,
             StatusTo = statusTo,
             UserId = userId,
