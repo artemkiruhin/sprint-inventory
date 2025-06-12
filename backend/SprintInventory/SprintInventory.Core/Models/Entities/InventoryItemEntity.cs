e57a6cc545c880c6037602497191333daca06dcc
artemkiruhin <temkaunik@gmail.com>
2025-06-12 17:10:29 +0300

    upd: set inventory item to rich model

--- backend/SprintInventory/SprintInventory.Core/Models/Entities/InventoryItemEntity.cs
+++ backend/SprintInventory/SprintInventory.Core/Models/Entities/InventoryItemEntity.cs
@@ -2,7 +2,14 @@
 
 public class InventoryItemEntity
 {
+    private const uint NameMinLength = 2;
+    private const uint NameMaxLength = 15;
+    private const uint DescriptionMinLength = 2;
+    
+    
     public Guid Id { get; set; }
+    public required string Name { get; set; }
+    public required string Description { get; set; }
     public string? InventoryNumber { get; set; }
     public string? SerialNumber { get; set; }
     public ItemStatus Status { get; set; }
@@ -16,4 +23,41 @@ public class InventoryItemEntity
     public virtual CategoryEntity? Category { get; set; } = null!;
     public virtual ICollection<InventoryItemMovementEntity> Movements { get; set; } = [];
     public virtual ICollection<InventoryItemCreatingLogEntity> CreatingLogs { get; set; } = [];
+
+    public static InventoryItemEntity Create(
+        string name,
+        string description,
+        string? inventoryNumber,
+        string? serialNumber,
+        ItemStatus status,
+        Guid? categoryId,
+        Guid? roomId,
+        Guid creatorId
+    )
+    {
+        
+        if (name.Trim().Length < NameMinLength || name.Trim().Length > NameMaxLength)
+            throw new ArgumentOutOfRangeException(
+                $"Name must be between {NameMinLength} and {NameMaxLength} characters long."
+            );
+        
+        if (description.Trim().Length < NameMinLength)
+            throw new ArgumentOutOfRangeException(
+                $"Description must be more than {DescriptionMinLength} characters long."
+            );
+        
+        return new()
+        {
+            Id = Guid.NewGuid(),
+            Name = name,
+            Description = description,
+            InventoryNumber = inventoryNumber,
+            SerialNumber = serialNumber,
+            Status = status,
+            CategoryId = categoryId,
+            RoomId = roomId,
+            CreatedAt = DateTime.UtcNow,
+            CreatorId = creatorId
+        };
+    } 
 }
