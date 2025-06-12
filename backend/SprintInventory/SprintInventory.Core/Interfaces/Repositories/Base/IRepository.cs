8be83617bbc46f5ca5ebc243879c312b7d1773c0
artemkiruhin <temkaunik@gmail.com>
2025-06-12 22:18:53 +0300

    add: base repo interface

---
+++ backend/SprintInventory/SprintInventory.Core/Interfaces/Repositories/Base/IRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace SprintInventory.Core.Interfaces.Repositories.Base;
+
+public interface IRepository<TEntity> where TEntity: class
+{
+    Task<Guid> Create(TEntity entity, CancellationToken ct);
+    Task<Guid> Update(TEntity updatedEntity, CancellationToken ct);
+    Task<Guid> Delete(Guid id, CancellationToken ct);
+    Task<TEntity?> GetById(Guid id, CancellationToken ct);
+    Task<IEnumerable<TEntity>> GetAll(CancellationToken ct);
+}
