cd77397a55639ff031b6732114b0c2ddd47bf03e
artemkiruhin <temkaunik@gmail.com>
2025-06-12 22:28:58 +0300

    add: UoW interface

---
+++ backend/SprintInventory/SprintInventory.Core/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,21 @@
+﻿using SprintInventory.Core.Interfaces.Repositories;
+
+namespace SprintInventory.Core.Interfaces;
+
+public interface IUnitOfWork
+{
+    IRoomRepository RoomRepository { get; }
+    ICategoryRepository CategoryRepository { get; }
+    ICreatingLogRepository CreatingLogRepository { get; }
+    IInventoryItemRepository InventoryItemRepository { get; }
+    IMovementRepository MovementRepository { get; }
+    IStatusLogRepository StatusLogRepository { get; }
+    IUserRepository UserRepository { get; }
+    
+    Task<int> SaveChangesAsync(CancellationToken ct);
+    Task BeginTransactionAsync(CancellationToken ct);
+    Task CommitTransactionAsync(CancellationToken ct);
+    Task RollbackTransactionAsync(CancellationToken ct);
+    
+    void Dispose();
+}
