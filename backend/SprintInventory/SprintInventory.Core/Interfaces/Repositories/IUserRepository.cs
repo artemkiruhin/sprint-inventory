090b868dd52154d3f0d854ce62de4e18e6edb0dc
artemkiruhin <temkaunik@gmail.com>
2025-06-12 22:21:22 +0300

    add: user repo

---
+++ backend/SprintInventory/SprintInventory.Core/Interfaces/Repositories/IUserRepository.cs
@@ -0,0 +1,12 @@
+﻿using SprintInventory.Core.Interfaces.Repositories.Base;
+using SprintInventory.Core.Models.Entities;
+
+namespace SprintInventory.Core.Interfaces.Repositories;
+
+public interface IUserRepository : IRepository<UserEntity>
+{
+    Task<IEnumerable<UserEntity>> GetBlockedUsers(CancellationToken ct);
+    Task<IEnumerable<UserEntity>> GetActiveUsers(CancellationToken ct);
+    Task<IEnumerable<UserEntity>> GetAdmins(CancellationToken ct);
+    Task<IEnumerable<UserEntity>> GetNonAdmins(CancellationToken ct);
+}
