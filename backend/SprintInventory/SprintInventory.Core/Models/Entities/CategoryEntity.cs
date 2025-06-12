bb5ce15f127094174c21c7a7e19ed683598f8144
artemkiruhin <temkaunik@gmail.com>
2025-06-12 17:13:30 +0300

    upd: set category to rich model

--- backend/SprintInventory/SprintInventory.Core/Models/Entities/CategoryEntity.cs
+++ backend/SprintInventory/SprintInventory.Core/Models/Entities/CategoryEntity.cs
@@ -2,6 +2,10 @@
 
 public class CategoryEntity
 {
+    private const uint NameMinLength = 3;
+    private const uint NameMaxLength = 20;
+    private const uint DescriptionMinLength = 2;
+    
     public Guid Id { get; set; }
     public required string Name { get; set; }
     public string? Description { get; set; }
@@ -10,4 +14,24 @@ public class CategoryEntity
     
     public virtual UserEntity Creator { get; set; } = null!;
     public virtual ICollection<InventoryItemEntity> InventoryItems { get; set; } = [];
+
+    public static CategoryEntity Create(string name, string? description, Guid creatorId)
+    {
+        if (name.Trim().Length < NameMinLength || name.Trim().Length > NameMaxLength)
+            throw new ArgumentOutOfRangeException(
+                $"Name must be between {NameMinLength} and {NameMaxLength} characters long."
+            );
+        
+        if (!string.IsNullOrWhiteSpace(description.Trim()) && description.Length < DescriptionMinLength)
+            throw new ArgumentOutOfRangeException($"Description must be more than {DescriptionMinLength} characters long.");
+        
+        return new()
+        {
+            Id = Guid.NewGuid(),
+            Name = name,
+            Description = description,
+            CreatedAt = DateTime.UtcNow,
+            CreatorId = creatorId
+        };
+    }
 }
