3311ebb8e85a6d5a7754e2af990944ef2d04a95b
artemkiruhin <temkaunik@gmail.com>
2025-06-13 12:28:13 +0300

    add: jwt service interface

---
+++ backend/SprintInventory/SprintInventory.Core/Interfaces/Services/Security/IJwtService.cs
@@ -0,0 +1,8 @@
+﻿using SprintInventory.Core.Models.Entities;
+
+namespace SprintInventory.Core.Interfaces.Services.Security;
+
+public interface IJwtService
+{
+    string GenerateToken(Guid userId);
+}
