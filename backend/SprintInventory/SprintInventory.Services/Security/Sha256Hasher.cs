c203fbae22c42702899ce8ef88983332a9aee3e9
artemkiruhin <temkaunik@gmail.com>
2025-06-13 20:15:43 +0300

    add: hasher impl as sha-256

---
+++ backend/SprintInventory/SprintInventory.Services/Security/Sha256Hasher.cs
@@ -0,0 +1,13 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+using SprintInventory.Core.Interfaces.Services.Security;
+
+namespace SprintInventory.Services.Security;
+
+public class Sha256Hasher : IHashService
+{
+    public string HashData(string data)
+    {
+        return Convert.ToHexStringLower(SHA256.HashData(Encoding.UTF8.GetBytes(data)));
+    }
+}
