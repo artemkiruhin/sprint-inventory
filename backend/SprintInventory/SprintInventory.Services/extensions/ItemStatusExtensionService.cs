bcdc4b2e985b93eec7cf85843cb6e67fad6bec96
artemkiruhin <temkaunik@gmail.com>
2025-06-13 21:10:08 +0300

    add: item status ext service

---
+++ backend/SprintInventory/SprintInventory.Services/extensions/ItemStatusExtensionService.cs
@@ -0,0 +1,40 @@
+﻿using SprintInventory.Core.Interfaces.Services.Extension;
+using SprintInventory.Core.Models.Entities;
+
+namespace SprintInventory.Services.Extensions;
+
+public class ItemStatusExtensionService : IItemStatusExtensionService
+{
+    public string GetStringName(ItemStatus itemStatus)
+    {
+        return itemStatus switch
+        {
+            ItemStatus.Normal => "Нормальное",
+            ItemStatus.Unused => "Не использованное",
+            ItemStatus.WrittenOff => "Списано",
+            _ => throw new ArgumentOutOfRangeException()
+        };
+    }
+
+    public ItemStatus GetStatusByNumber(int number)
+    {
+        return number switch
+        {
+            0 => ItemStatus.Normal,
+            1 => ItemStatus.Unused,
+            2 => ItemStatus.WrittenOff,
+            _ => throw new ArgumentOutOfRangeException()
+        };
+    }
+
+    public ItemStatus GetItemStatusByStringName(string stringName)
+    {
+        return stringName switch
+        {
+            "Нормальное" => ItemStatus.Normal,
+            "Не использовано" => ItemStatus.Unused,
+            "Списано" => ItemStatus.WrittenOff,
+            _ => throw new ArgumentOutOfRangeException()
+        };
+    }
+}
