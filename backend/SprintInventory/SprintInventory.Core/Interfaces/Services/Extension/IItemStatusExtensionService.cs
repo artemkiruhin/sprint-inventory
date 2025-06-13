de844724676e387418d5c6c9ceedce4ee31c5d31
artemkiruhin <temkaunik@gmail.com>
2025-06-13 12:25:50 +0300

    add: item status ext service

---
+++ backend/SprintInventory/SprintInventory.Core/Interfaces/Services/Extension/IItemStatusExtensionService.cs
@@ -0,0 +1,10 @@
+﻿using SprintInventory.Core.Models.Entities;
+
+namespace SprintInventory.Core.Interfaces.Services.Extension;
+
+public interface IItemStatusExtensionService
+{
+    string GetStringName(ItemStatus itemStatus);
+    ItemStatus GetStatusByNumber(int number);
+    ItemStatus GetItemStatusByStringName(string stringName);
+}
