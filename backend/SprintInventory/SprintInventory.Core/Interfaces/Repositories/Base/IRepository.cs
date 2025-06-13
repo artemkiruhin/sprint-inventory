690cb037221e7da820f82c3d25789ce806af8e2c
artemkiruhin <temkaunik@gmail.com>
2025-06-13 19:53:37 +0300

    upd: repository base interface minor changes

--- backend/SprintInventory/SprintInventory.Core/Interfaces/Repositories/Base/IRepository.cs
+++ backend/SprintInventory/SprintInventory.Core/Interfaces/Repositories/Base/IRepository.cs
@@ -4,9 +4,9 @@ namespace SprintInventory.Core.Interfaces.Repositories.Base;
 
 public interface IRepository<TEntity> where TEntity: class
 {
-    Task<Guid> Create(TEntity entity, CancellationToken ct);
-    Task<Guid> Update(TEntity updatedEntity, CancellationToken ct);
-    Task<Guid> Delete(Guid id, CancellationToken ct);
+    Task<TEntity> Create(TEntity entity, CancellationToken ct);
+    TEntity Update(TEntity updatedEntity, CancellationToken ct);
+    Task<TEntity> Delete(Guid id, CancellationToken ct);
     Task<TEntity?> GetById(Guid id, CancellationToken ct);
     Task<IEnumerable<TEntity>> GetAll(CancellationToken ct);
     Task<IEnumerable<TEntity>> Search(Expression<Func<TEntity, bool>> predicate, CancellationToken ct);
