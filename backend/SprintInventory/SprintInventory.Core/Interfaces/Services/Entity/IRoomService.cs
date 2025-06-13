a3cbc35143e68307029e0d4e03381a1bde81061e
artemkiruhin <temkaunik@gmail.com>
2025-06-13 12:12:22 +0300

    add: room service interface

---
+++ backend/SprintInventory/SprintInventory.Core/Interfaces/Services/Entity/IRoomService.cs
@@ -0,0 +1,19 @@
+﻿using SprintInventory.Core.Models.Contracts.Create;
+using SprintInventory.Core.Models.Contracts.Delete;
+using SprintInventory.Core.Models.Contracts.Specific;
+using SprintInventory.Core.Models.Contracts.Update;
+using SprintInventory.Core.Models.DTOs;
+using SprintInventory.Core.Models.DTOs.Detailed;
+using SprintInventory.Core.Models.DTOs.Short;
+
+namespace SprintInventory.Core.Interfaces.Services.Entity;
+
+public interface IRoomService
+{
+    Task<Result<Guid>> Create(RoomCreateContract request, CancellationToken ct);
+    Task<Result<Guid>> Update(RoomUpdateContract request, CancellationToken ct);
+    Task<Result<Guid>> Delete(BaseDeleteContract request, CancellationToken ct);
+    Task<Result<List<RoomDetailedDTO>>> GetAllRoomsDetailed(CancellationToken ct);
+    Task<Result<List<RoomShortDTO>>> GetAllRoomsShort(CancellationToken ct);
+    Task<Result<RoomDetailedDTO>> GetRoomById(Guid id, CancellationToken ct);
+}
