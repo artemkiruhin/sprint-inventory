3d0dbf348fad0422255ce48b984b25ad804b8ca2
artemkiruhin <temkaunik@gmail.com>
2025-06-13 12:21:08 +0300

    add: category service interface

---
+++ backend/SprintInventory/SprintInventory.Core/Interfaces/Services/Entity/ICategoryService.cs
@@ -0,0 +1,18 @@
+﻿using SprintInventory.Core.Models.Contracts.Create;
+using SprintInventory.Core.Models.Contracts.Delete;
+using SprintInventory.Core.Models.Contracts.Update;
+using SprintInventory.Core.Models.DTOs;
+using SprintInventory.Core.Models.DTOs.Detailed;
+using SprintInventory.Core.Models.DTOs.Short;
+
+namespace SprintInventory.Core.Interfaces.Services.Entity;
+
+public interface ICategoryService
+{
+    Task<Result<Guid>> Create(CategoryCreateContract request, CancellationToken ct);
+    Task<Result<Guid>> Update(CategoryUpdateContract request, CancellationToken ct);
+    Task<Result<Guid>> Delete(BaseDeleteContract request, CancellationToken ct);
+    Task<Result<List<CategoryDetailedDTO>>> GetAllCategoriesDetailed(CancellationToken ct);
+    Task<Result<List<CategoryShortDTO>>> GetAllCategoriesShort(CancellationToken ct);
+    Task<Result<CategoryDetailedDTO>> GetCategoryById(Guid id, CancellationToken ct);
+}
