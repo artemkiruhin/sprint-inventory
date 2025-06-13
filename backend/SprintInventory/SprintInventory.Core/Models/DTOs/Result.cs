4cc47d76d1b664b43b0c4305be3c58e57b6294eb
artemkiruhin <temkaunik@gmail.com>
2025-06-13 10:57:05 +0300

    add: result DTO

---
+++ backend/SprintInventory/SprintInventory.Core/Models/DTOs/Result.cs
@@ -0,0 +1,14 @@
+﻿namespace SprintInventory.Core.Models.DTOs;
+
+public class Result<TResult>
+{
+    public TResult? Data { get; set; }
+    public string? ErrorMessage { get; set; }
+    public bool IsSuccess { get; init; }
+
+    public Result<TResult> Success(TResult data)
+        => new() { Data = data, IsSuccess = IsSuccess, ErrorMessage = string.Empty };
+
+    public Result<TResult> Failure(string errorMessage)
+        => new() { IsSuccess = false, ErrorMessage = errorMessage };
+}
