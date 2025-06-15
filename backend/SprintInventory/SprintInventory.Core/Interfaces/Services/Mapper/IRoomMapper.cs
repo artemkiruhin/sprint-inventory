952fef94880a8b8ab6bd1e5a800ab8525ea9c82e
artemkiruhin <temkaunik@gmail.com>
2025-06-15 22:24:55 +0300

    add: room mapper interface

---
+++ backend/SprintInventory/SprintInventory.Core/Interfaces/Services/Mapper/IRoomMapper.cs
@@ -0,0 +1,13 @@
+﻿using SprintInventory.Core.Models.DTOs.Detailed;
+using SprintInventory.Core.Models.DTOs.Short;
+using SprintInventory.Core.Models.Entities;
+
+namespace SprintInventory.Core.Interfaces.Services.Mapper;
+
+public interface IRoomMapper
+{
+    List<RoomDetailedDTO> MapToDetailedDTORange(List<RoomEntity> roomEntities);
+    RoomDetailedDTO MapToDetailedDTO(RoomEntity roomEntity);
+    RoomShortDTO MapToShortDTO(RoomEntity roomEntity);
+    List<RoomShortDTO> MapToShortDTORange(List<RoomEntity> roomEntities);
+}
