11a918e76aef1d35a28354b3869d129fc75221d3
artemkiruhin <temkaunik@gmail.com>
2025-06-15 20:51:00 +0300

    add: user mapper interface

---
+++ backend/SprintInventory/SprintInventory.Core/Interfaces/Services/Mapper/IUserMapper.cs
@@ -0,0 +1,13 @@
+﻿using SprintInventory.Core.Models.DTOs.Detailed;
+using SprintInventory.Core.Models.DTOs.Short;
+using SprintInventory.Core.Models.Entities;
+
+namespace SprintInventory.Core.Interfaces.Services.Mapper;
+
+public interface IUserMapper
+{
+    UserDetailedDTO MapToDetailedDTO(UserEntity userEntity);
+    List<UserDetailedDTO> MapToDetailedDTORange(List<UserEntity> userEntities);
+    List<UserShortDTO> MapToShortDTORange(List<UserEntity> userEntities);
+    UserShortDTO MapToShortDTO(UserEntity userEntity);
+}
