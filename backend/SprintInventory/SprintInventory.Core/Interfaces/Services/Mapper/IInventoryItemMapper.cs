60c3813f3a4070d4875a4840be7f627f804168e8
artemkiruhin <temkaunik@gmail.com>
2025-06-16 09:04:59 +0300

    add: inventory mapper interface

---
+++ backend/SprintInventory/SprintInventory.Core/Interfaces/Services/Mapper/IInventoryItemMapper.cs
@@ -0,0 +1,13 @@
+﻿using SprintInventory.Core.Models.DTOs.Detailed;
+using SprintInventory.Core.Models.DTOs.Short;
+using SprintInventory.Core.Models.Entities;
+
+namespace SprintInventory.Core.Interfaces.Services.Mapper;
+
+public interface IInventoryItemMapper
+{
+    InventoryItemDetailedDTO MapToDetailedDTO(InventoryItemEntity itemEntity);
+    List<InventoryItemDetailedDTO> MapToDetailedDTORange(List<InventoryItemEntity> itemEntities);
+    InventoryItemShortDTO MapToShortDTO(InventoryItemEntity itemEntity);
+    List<InventoryItemShortDTO> MapToShortDTORange(List<InventoryItemEntity> itemEntities);
+}
