8aaad45a2780ea1872346634712c5de7b77e1490
artemkiruhin <temkaunik@gmail.com>
2025-06-16 08:48:40 +0300

    add: category mapper interface

---
+++ backend/SprintInventory/SprintInventory.Core/Interfaces/Services/Mapper/ICategoryMapper.cs
@@ -0,0 +1,13 @@
+﻿using SprintInventory.Core.Models.DTOs.Detailed;
+using SprintInventory.Core.Models.DTOs.Short;
+using SprintInventory.Core.Models.Entities;
+
+namespace SprintInventory.Core.Interfaces.Services.Mapper;
+
+public interface ICategoryMapper
+{
+    CategoryShortDTO MapToCategoryShortDTO(CategoryEntity categoryEntity);
+    List<CategoryShortDTO> MapToCategoryShortDTORange(List<CategoryEntity> categoryEntities);
+    CategoryDetailedDTO MapToCategoryDetailedDTO(CategoryEntity categoryEntity);
+    List<CategoryDetailedDTO> MapToCategoryDetailedDTORange(List<CategoryEntity> categoryEntities);
+}
