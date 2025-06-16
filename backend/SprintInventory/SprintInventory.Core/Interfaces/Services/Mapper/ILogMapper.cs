888f42c7bd32e703654ac613f0964a4a40908418
artemkiruhin <temkaunik@gmail.com>
2025-06-16 09:16:52 +0300

    add: log mapper interface

---
+++ backend/SprintInventory/SprintInventory.Core/Interfaces/Services/Mapper/ILogMapper.cs
@@ -0,0 +1,14 @@
+﻿using SprintInventory.Core.Models.DTOs.Detailed;
+using SprintInventory.Core.Models.DTOs.Short;
+using SprintInventory.Core.Models.Entities;
+
+namespace SprintInventory.Core.Interfaces.Services.Mapper;
+
+public interface ILogMapper
+{
+    List<CreatingLogShortDTO> MapCreatingLogShortDTORange(List<InventoryItemCreatingLogEntity> logEntities);
+    List<CreatingLogDetailedDTO> MapCreatingLogDetailedDTORange(List<InventoryItemCreatingLogEntity> logEntities);
+    List<StatusLogDetailedDTO> MapStatusLogDetailedDTORange(List<InventoryItemStatusLogEntity> logEntities);
+    List<MovementDetailedDTO> MapMovementDetailedDTORange(List<InventoryItemMovementEntity> logEntities);
+    
+}
