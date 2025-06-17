8f2e30898a4e55538de9aaf6de43dde1d6ce529c
artemkiruhin <temkaunik@gmail.com>
2025-06-17 08:24:57 +0300

    add: inventory item repo impl

---
+++ backend/SprintInventory/SprintInventory.Infrastructure/Repositories/InventoryItemRepository.cs
@@ -0,0 +1,10 @@
+﻿using SprintInventory.Core.Interfaces.Repositories;
+using SprintInventory.Core.Models.Entities;
+using SprintInventory.Infrastructure.Repositories.Base;
+
+namespace SprintInventory.Infrastructure.Repositories;
+
+public class InventoryItemRepository(AppDbContext context) : BaseRepository<InventoryItemEntity>(context), IInventoryItemRepository
+{
+    
+}
